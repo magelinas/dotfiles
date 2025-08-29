3dc90e40aba01bc7bd513be1b4a68dca5e318da4
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-08-29 15:23:14 -0400

    Add lensline and codecompanion

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,36 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+        strategies = {
+            -- Change the default chat adapter and model
+            chat = {
+                adapter = "qwen",
+            },
+        },
+        adapters = {
+            http = {
+                qwen = function()
+                    return require("codecompanion.adapters").extend("ollama", {
+                        name = "qwen",
+                        schema = {
+                            model = {
+                                default = "qwen3-coder:30b",
+                            },
+                            keep_alive = {
+                                default = "5m",
+                            },
+                        },
+                    })
+                end,
+            },
+        },
+        -- NOTE: The log_level is in `opts.opts`
+        opts = {
+            log_level = "DEBUG",
+        },
+    },
+}
---
+++ .config/nvim/lua/plugins/lensline.lua
@@ -0,0 +1,6 @@
+return {
+    "oribarilan/lensline.nvim",
+    tag = "release/1.x", -- or: branch = 'release/1.x' for latest non-breaking updates
+    event = "LspAttach",
+    opts = {},
+}

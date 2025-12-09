107ffd5e27ef2381b7af2bcb1417920bb343697b
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2025-12-08 21:42:43 -0500

    Replace fugitive with neogit and add cspell

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -2,11 +2,28 @@ return {
     "nvimtools/none-ls.nvim",
     dependencies = {
         "nvimtools/none-ls-extras.nvim",
+        "davidmh/cspell.nvim",
     },
+    opts = function(_, opts)
+        local cspell = require("cspell")
+        opts.sources = opts.sources or {}
+        table.insert(
+            opts.sources,
+            cspell.diagnostics.with({
+                diagnostics_postprocess = function(diagnostic)
+                    diagnostic.severity = vim.diagnostic.severity.HINT
+                end,
+            })
+        )
+        table.insert(opts.sources, cspell.code_actions)
+    end,
     config = function()
         local null_ls = require("null-ls")
+        local cspell = require("cspell")
         null_ls.setup({
             sources = {
+                cspell.diagnostics,
+                cspell.code_actions,
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.gofumpt,

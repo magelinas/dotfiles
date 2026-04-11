a09d1667044b78b72e03aff9ee29b99352648990
Marc-Antoine Gelinas <marc-antoine.gelinas@outlook.com>
2026-04-10 20:20:30 -0400

    Remove go stuff

--- .config/nvim/plugin/none-ls.lua
+++ .config/nvim/plugin/none-ls.lua
@@ -29,14 +29,8 @@ null_ls.setup({
         cspell.diagnostics,
         cspell.code_actions,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.diagnostics.golangci_lint,
         require("none-ls.diagnostics.ruff"),
     },
 })
 
-vim.keymap.set("n", "<leader>gf>", vim.lsp.buf.format, { desc = "Buffer format" } )
+vim.keymap.set("n", "<leader>gf>", vim.lsp.buf.format, { desc = "Buffer format" })
